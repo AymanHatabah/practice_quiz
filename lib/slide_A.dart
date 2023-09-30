8e04001713a3e07fc95f1d458169a7ce57483c0d
Ayman <ayman.hatabah55@>
2023-09-30 13:33:13 +0300

    app init

--- lib/slide_A.dart
+++ lib/slide_A.dart
@@ -6,13 +6,48 @@ class FiTest extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("T"),
-      ),
-      body: Column(
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Column(
+    children: [
+
+      Row(
         children: [
-      ]
+        CircleAvatar(
+        radius: 30,
+  child: Image.asset("assets/images/img_14.png"),
+  ),
+
+          Column(
+
+            children: [
+              Text("hello,jeda",style: TextStyle(fontSize: 20),),
+              SizedBox(
+                height: 10,
+              ),
+
+              Text("Ready to Workout?",style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold),)
+            ],
+          ),
+          SizedBox(
+            width: 31,
+          ),
+
+          Badge(
+            label: Text(""),
+            child: Icon(
+              Icons.notification_add,
+              size: 30,
+            ),
+          ),
+
+        ],
+        )
+  ],
+    ),
       ),
+
+
     );
   }
 }
